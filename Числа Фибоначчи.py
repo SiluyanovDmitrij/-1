09b40d7a9f116f37d3c9fbfc8a080e1a87c8577a
SiluyanovDmitrij <113800727+SiluyanovDmitrij@users.noreply.github.com>
2022-09-17 23:01:51 +0300

    Add files via upload

---
+++ Числа Фибоначчи.py
@@ -0,0 +1,14 @@
+print('Числа Фибоначчи')
+print()
+print()
+fib = lambda n: 0 if n < 1 else 1 if n == 1 else fib(n-1) + fib(n-2)
+x = int(input('Введите число: '))
+n = 0
+while x > fib(n):
+    n += 1
+if fib(n) == x:
+    print('Число является числом Фибоначчи')
+else:
+    print('Число не является числом Фиббоначи')
+
+
